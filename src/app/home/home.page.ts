001eba23593a2855547110bde3151e9861c99ce6
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2019-08-16 21:20:47 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public items : any;
+  constructor(public http: HttpClient) {
+    this.getData();
+  }
 
+
+  getData() {
+    let url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+    let data: Observable<any> = this.http.get(url);
+    data.subscribe(result => {
+      console.log(result);
+      this.items = result;
+    });
+  }
 }
