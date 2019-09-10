21b131982a42561e4262dfb97cb185a75c4177a0
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2019-09-10 21:14:56 +0530

    Working with tokens

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -9,18 +9,41 @@ import { Observable } from 'rxjs';
 })
 export class HomePage {
 
-  public items : any;
+  public Token : string;
+
+
+  //public items : any;
   constructor(public http: HttpClient) {
-    this.getData();
+    //this.getData();
+    this.sample();
   }
 
+  sample() {
+    this.Token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY4MTI5MjA3LCJqdGkiOiIyMzE0NjY4ZTU4NjI0MWI3ODExNjQzNzQ4NWFlNzEyMiIsInVzZXJfaWQiOjF9.MrIeBR-anpKsGs4ZlP2rcSQUsKunZlkvM2MksP0tbWY';
+    let headers = new HttpHeaders();
+    let turl = 'https://djangorestapiionic.herokuapp.com/users/';
+    headers.append('Authorization', 'Bearer ' + this.Token);
+    let data: Observable<any> = this.http.get(turl,{headers: headers});
+      data.subscribe(result => {
+      console.log(result);
+    });
+  }
 
+  /*
   getData() {
     let url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+    //let turl = 'http://127.0.0.1:8000/users/';
+    let turl = 'https://djangorestapiionic.herokuapp.com/users';
     let data: Observable<any> = this.http.get(url);
-    data.subscribe(result => {
+     data.subscribe(result => {
       console.log(result);
       this.items = result;
     });
+    
+    let tdata: Observable<any> = this.http.get(turl);
+    tdata.subscribe(result => {
+      console.log(result);
+    });
   }
+  */
 }
