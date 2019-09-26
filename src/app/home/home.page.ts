34705610376ea35eb4582453a10788994ffbad8d
P Sai Bhaskar <saibaaskar24091999@gmail.com>
2019-09-26 06:30:30 +0530

    Next changes will be reflected in diffent branch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,16 +9,17 @@ import { Observable } from 'rxjs';
 })
 export class HomePage {
 
-  public Token : string;
+ // public Token : string;
+ public items : any ;
 
 
   //public items : any;
   constructor(public http: HttpClient) {
-    //this.getData();
-    this.sample();
+    this.getData();
+    //this.sample();
   }
 
-  sample() {
+ /* sample() {
     this.Token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY4MTI5MjA3LCJqdGkiOiIyMzE0NjY4ZTU4NjI0MWI3ODExNjQzNzQ4NWFlNzEyMiIsInVzZXJfaWQiOjF9.MrIeBR-anpKsGs4ZlP2rcSQUsKunZlkvM2MksP0tbWY';
     let headers = new HttpHeaders();
     let turl = 'https://djangorestapiionic.herokuapp.com/users/';
@@ -27,23 +28,25 @@ export class HomePage {
       data.subscribe(result => {
       console.log(result);
     });
-  }
+  } 
+  */
 
-  /*
+  
   getData() {
     let url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
     //let turl = 'http://127.0.0.1:8000/users/';
-    let turl = 'https://djangorestapiionic.herokuapp.com/users';
+    //let turl = 'https://djangorestapiionic.herokuapp.com/users';
     let data: Observable<any> = this.http.get(url);
      data.subscribe(result => {
       console.log(result);
       this.items = result;
     });
     
-    let tdata: Observable<any> = this.http.get(turl);
+    /*let tdata: Observable<any> = this.http.get(turl);
     tdata.subscribe(result => {
       console.log(result);
     });
+    */
   }
-  */
+
 }
